a3a4f956ab7373d9fd6bdb201d2c4c879c6a72e3
AndreasLoukakis <alou.gr@gmail.com>
2020-04-03 12:25:27 +0300

    missing mortgage factory cmp

---
+++ src/app/common/lazy/mortgage-factory-product/mortgage-factory-product.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, NgModule } from '@angular/core';
+import { ComplexBaseComponent } from './../../../stk/abstract/complex-base-component';
+import { ApiService } from './../../../stk/services/api.service';
+import { SharedModule } from 'src/app/shared/shared.module';
+
+@Component({
+  templateUrl: './mortgage-factory-product.component.html',
+  styleUrls: ['./mortgage-factory-product.component.scss'],
+  providers: [{provide: ComplexBaseComponent, useExisting: MortgageFactoryProductComponent}]
+})
+export class MortgageFactoryProductComponent extends ComplexBaseComponent {
+
+  constructor(api: ApiService) { super(api); }
+
+}
+
+@NgModule({
+  declarations: [MortgageFactoryProductComponent],
+  imports: [SharedModule],
+  providers: []
+})
+class MortgageFactoryProductModule {}
